87d169ecf7d09417954c4f89574f216bbe47e867
Janzie21 <nick_janssen21@hotmail.com>
2022-05-05 22:08:00 +0200

    Update contact form

--- process.php
+++ process.php
@@ -9,7 +9,7 @@
     $message = $_POST['message'];
     
     //send email
-    mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
+    mail($admin_email, "New Form Submission", $message . ' - ' 'Name: ' $name . 'Phone number: ' . $phone, "From:" . $email);
     
     header('Location: https://www.kelderdeuntjes.be/success.html');
   }
